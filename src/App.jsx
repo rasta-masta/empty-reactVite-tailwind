de51dbdd9b381dc2b4188832bcfd9ded8326a52a
rasta-masta <143915828+rasta-masta@users.noreply.github.com>
2023-10-03 01:06:56 +0700

    react-vite+tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+  
+      <div>
+        <h1 className='text-6xl font-bold underline'>Welcome</h1>
+      </div>
+     
+   
+  )
+}
+
+export default App
